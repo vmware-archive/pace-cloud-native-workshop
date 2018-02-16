7553caac4afa20d3b9a5725b911a5eab046756b5
Daniel L. Buchko <dbuchko@pivotal.io>
2018-02-15 23:36:46 -0500

    Updates for demo.

--- sb-basic-demo/src/main/java/io/pivotal/pace/SBController.java
+++ sb-basic-demo/src/main/java/io/pivotal/pace/SBController.java
@@ -3,10 +3,12 @@ package io.pivotal.pace;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class SBController {
 	
 	private GreetingRepository greetingRepository;
